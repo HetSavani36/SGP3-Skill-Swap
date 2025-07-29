ee5504933cbc862bdaaf0eb7e1ca738df57f4ecf
HetSavani36 <hetsavani36@gmail.com>
2025-07-29 17:20:25 +0530

    first commit basic structure

---
+++ Backend/src/models/user.model.js
@@ -0,0 +1,25 @@
+const mongoose=require('mongoose')
+
+const userSchema=mongoose.Schema({
+
+    username:{
+        type:String,
+        trim:true,
+        unique:true,
+        lowercase:true,
+        required:true
+    },
+    fullname:{
+        type:String,
+        trim:true,
+        required:true
+    },
+    email:{
+        type:String,
+        trim:true,
+        lowercase:true,
+        required:true,
+        
+    }
+
+},{timeStamps:true})
