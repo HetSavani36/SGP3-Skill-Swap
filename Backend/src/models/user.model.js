b2404725e0e73f5a407e39ce70fe555fa2881d71
HetSavani36 <hetsavani36@gmail.com>
2025-07-29 18:00:30 +0530

    added user model

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose=require('mongoose')
+const validator = require('validator')
 
 const userSchema=mongoose.Schema({
 
@@ -19,9 +20,63 @@ const userSchema=mongoose.Schema({
         trim:true,
         lowercase:true,
         required:true,
-        
-
-        
+        unique:true,
+        validate:[validator.isEmail,"Invalid email format"]
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    profilePhoto:{
+        type:String,  //cloudinary url
+        default:"https://i.ibb.co/2kRrzBJ/default-profile.png"
+    },
+    coverPhoto:{
+        type:String,  //cloudinary url
+        default:"https://i.ibb.co/2kRrzBJ/default-profile.png"
+    },
+    dob:{
+        type:Date,
+        required:true,
+        validate:{
+            validator: function (value) {
+                return value <= new Date();
+            },
+            message: "DOB cannot be in the future.",
+        }
+    },
+    age:{
+        type:Number,
+        default:0
+    },
+    location:{
+        type:String,
+        trim:true
+    },
+    isAdmin:{
+        type:Boolean,
+        default:false
+    },
+    refreshToken:{
+        type:String,
     }
-
 },{timeStamps:true})
+
+userSchema.pre("save",async function (next) {
+    try {
+        const today = new Date();
+        this.age = today.getFullYear() - this.dob.getFullYear();
+        if (
+            today.getMonth() < this.dob.getMonth() ||
+            (today.getMonth() === this.dob.getMonth() &&
+            today.getDate() < this.dob.getDate())
+        ) {
+            this.age--;
+        }
+        next()
+    } catch (error) {
+        next(error)
+    }
+})
+
+module.exports=mongoose.model("User",userSchema)
